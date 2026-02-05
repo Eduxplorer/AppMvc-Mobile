058587a1ffce9a3a5cd66bbf64fe395706517dc4
Eduxplorer <eduardods.silva@outlook.com>
2026-02-05 10:36:45 -0300

    Adiciona a barra de navegação e refatora a tela inicial para ocultar o cabeçalho

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -11,7 +11,7 @@ export default function AppRoutes() {
     return(
         <Stack.Navigator>
             {/* <Stack.Screen name="Home" component={HomeView} options={{ headerShown: false }} /> Esse bloco é utilizado para caso você desejar ocultar a barra com o nome da página que fica na parte superior */}
-            <Stack.Screen name="Home" component={HomeView} options={{ title: 'Minha Home' }} />
+            <Stack.Screen name="Home" component={HomeView} options={{ headerShown: false }} />
             <Stack.Screen name="Login" component={LoginView} options={{ title: 'Meu Login' }} />
             <Stack.Screen name="Cadastro"  component={CadastroView} options={{title: 'Cadastre-se'}} />
             <Stack.Screen name="EsqueciSenha" component={EsqueciSenhaView} options={{title: "Recuperar Conta"}} />
--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,11 +1,15 @@
-import {Button, Text} from 'react-native';
+import { Button, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import HeaderBar from '../components/HeaderBar';
 
-export default function HomeView({navigation}) {
-    return(
-        <SafeAreaView style={{flex: 1}}>
-        <Text>Olá Mundo! Estamos na página Home.</Text>
-        <Button title='Login' onPress={() => navigation.navigate('Login')} />
+export default function HomeView({ navigation }) {
+    return (
+        <SafeAreaView style={{ flex: 1 }}>
+
+            {/* Precisamos passar o navigation via props pois o componente não possui acesso direto. Apenas os componentes que estão definidos no AppRoutes possuem acesso ao navigation por padrão. */}
+            <HeaderBar navigation={navigation} />
+            {/* <Text>Olá Mundo! Estamos na página Home.</Text> */}
+            <Button title='Login' onPress={() => navigation.navigate('Login')} />
         </SafeAreaView>
     );
 }
