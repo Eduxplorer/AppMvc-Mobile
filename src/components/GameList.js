41469a9990a979b5f50178ac92ba47a29faa28ae
Eduxplorer <eduardods.silva@outlook.com>
2026-02-06 12:03:26 -0300

    Adiciona ícone de avaliação e exibe desconto na lista de jogos, além de refatorar o componente GameItem

--- src/components/GameList.js
+++ src/components/GameList.js
@@ -1,6 +1,6 @@
 import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-import {} from '@expo/vector-icons'
+import { AntDesign } from '@expo/vector-icons'
 
 const icon = '../../assets/icon.png'
 
@@ -10,52 +10,71 @@ const GAMES = [
         id: '1',
         title: 'The Last Guardian',
         price: 'R$199',
-        image:require(icon)
+        discount: '10%',
+        rating: 4.5,
+        image: require(icon)
     },
     {
         id: '2',
         title: 'Galactic Odyssey',
         price: 'R$199',
-        image:require(icon)
+        image: require(icon)
     },
     {
         id: '3',
         title: 'Costroms',
         price: 'R$199',
-        image:require(icon)
+        image: require(icon)
     },
     {
         id: '4',
         title: 'Orbital Froniter',
         price: 'R$199',
-        image:require(icon)
+        image: require(icon)
     }
 ]
 
-const  GameItem = ({item}) => {
+const GameItem = ({ item }) => {
 
-    return(
-    <TouchableOpacity>
-        <Image source={item.image} />
-        <View>
-            <Text>{item.price}</Text>
-        </View>
-        <Text>{item.price}</Text>
-    </TouchableOpacity>
+    return (
+        <TouchableOpacity style={styles.itemContainer}>
+            <Image style={styles.gameImage} source={item.image} />
+
+            {item.discount && (
+                <View style={styles.discountContainer}>
+                    <Text style={styles.discountText}>{item.discount}</Text>
+                </View>
+            )}
+            <View style={styles.gamePriceContainer}>
+                <Text style={styles.gamePriceText}>{item.price}</Text>
+            </View>
+            <Text style={styles.gameTitle}>{item.title}</Text>
+
+
+            {item.rating && (
+                <View>
+                    {Array(Math.round(item.rating)).fill(0).map((_, i) => (
+
+                    <AntDesign key={i} name='star' size={12} color="#ffd700" />
+                    ))}
+
+                </View>
+            )}
+        </TouchableOpacity>
     );
 
 
 }
 
 
-export default function GameList({title}) {
+export default function GameList({ title }) {
 
-    return(
+    return (
         <View>
-            <Text>{title}</Text>
+            <Text style={styles.title}>{title}</Text>
             <FlatList
                 data={GAMES}
-                renderItem={({item}) => <GameItem item={item} />}
+                renderItem={({ item }) => <GameItem item={item} />}
                 keyExtractor={item => item.id}
                 horizontal={true}
                 showsHorizontalScrollIndicator={false}
@@ -70,10 +89,59 @@ export default function GameList({title}) {
             */}
         </View>
     );
-} 
+}
 
 const styles = StyleSheet.create({
+    itemContainer: {
+        width: 120,
+        marginRight: 15,
+
+    },
+
+
+    gameImage: {
+        width: '100%',
+        height: 150,
+        borderRadius: 8,
+        marginBottom: 2
+    },
+
+    gamePriceContainer: {
+        alignItems: 'center'
+    },
+
+    gamePriceText: {
+        color: '#ffa500',
+        fontSize: 14,
+        fontWeight: 'bold'
+    },
+
+    gameTitle: {
+        color: '#fff',
+        fontSize: 12,
+        marginBottom: 3,
+        textAlign: 'center'
+    },
+
+    discountContainer: {
+        position: 'absolute',
+        top: 5,
+        left: 5,
+        backgroundColor: '#9370db',
+        paddingHorizontal: 5,
+        paddingVertical: 2,
+        borderRadius: 5,
+        zIndex: 10
+    },
+
+    discountText: {
+        color: '#fff',
+        fontSize: 10,
+        fontWeight: 'bold'
+    },
+
     listContent: {
         paddingHorizontal: 15,
-    }
+    },
+
 });
