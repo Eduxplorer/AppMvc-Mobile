dd6b3536a9f0a3dd2483cbeed7d2592165f8a5cd
Eduxplorer <eduardods.silva@outlook.com>
2026-04-07 12:04:26 -0300

    Implementa lógica de recuperação de senha na ForgotPassView
    - Adiciona validação para campo de email antes de enviar solicitação
    - Implementa chamada à API para envio de email de recuperação de senha
    - Adiciona tratamento de erros e exibição de mensagens de alerta
    - Atualiza o componente para gerenciar o estado do email

--- src/views/ForgotPassView.js
+++ src/views/ForgotPassView.js
@@ -1,17 +1,55 @@
-import { Text, View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, StyleSheet, Alert } from 'react-native';
+import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ForgotPassView({ navigation }) {
+
+    const [email, setEmail] = useState('');
+    const API_BASE_URL = "http://10.0.2.2:5203/api/Account/forgot-password";
+
+    const handleForgot = async () => {
+
+        if (!email) {
+            Alert.alert("Atenção", "Preencha seu Email.");
+            return;
+        }
+
+        try {
+            const response = await fetch(`${API_BASE_URL}`,
+                {
+                    method: 'POST',
+                    headers: { "Content-Type": "application/json" },
+                    body: email
+                }
+            );
+
+            if (response.ok) {
+                Alert.alert("Senha Enviada com Sucesso", "Verifique sua caixa de Email");
+                navigation.navigate('EsqueciSenha')
+            } else {
+                const erroData = response.json();
+                const erroMessage = erroData.message || "Erro ao enviar Email.";
+                Alert.alert('Erro', erroMessage);
+            }
+        } catch (error) {
+            console.log("Erro ao enviar Email", error);
+            Alert.alert("Ocorreu um erro!", `Erro ao enviar email: ${error}`);
+
+        }
+    }
+
+
+
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <View style={styles.container}>
                 <Text style={styles.title}>Esqueci a senha</Text>
                 <Text style={styles.descricao}>Informe seu e-mail cadastrado para receber as instruções de recuperação de senha.</Text>
 
-                <TextInput style={styles.input} placeholder='Email'></TextInput>
-                <TouchableOpacity style={styles.button}>
+                <TextInput style={styles.input} value={email} onChangeText={setEmail} placeholder='Email'></TextInput>
+                <TouchableOpacity style={styles.button} onPress={handleForgot}>
                     <Text style={styles.buttonText}>Enviar</Text>
-                </TouchableOpacity>               
+                </TouchableOpacity>
 
                 <TouchableOpacity onPress={() => navigation.goBack()}>
                     <Text style={styles.link}>Voltar para o login</Text>
